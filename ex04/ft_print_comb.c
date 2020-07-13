a6e191917acbcb4335a08388130d3247e742c388
Vernon Everts <veeverts@c5r3s3.wethinkcode.co.za>
2020-07-13 11:59:24 +0200

    ex04

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    //1
+    int i, j, k;
+    int arr[3];
+    //2
+    printf("1 : ");
+    scanf("%d", &arr[0]);
+    printf("2 : ");
+    scanf("%d", &arr[1]);
+    printf("3 : ");
+    scanf("%d", &arr[2]);
+    //3
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 3; k++)
+            {
+                if (i != j && j != k && k != i)
+                {
+                    //4
+                    printf("[%d %d %d]\n", arr[i], arr[j], arr[k]);
+                }
+            }
+        }
+    }
+}
